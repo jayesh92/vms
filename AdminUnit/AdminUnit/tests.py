b2c88dd9c1ad188feaa8cd0f1845ebc92d5dfe78
jayesh92 <jayesh.lahori.11.8.92@gmail.com>
2014-07-24 23:56:53 +0530

    Reset to default

--- AdminUnit/AdminUnit/tests.py
+++
@@ -1,7 +0,0 @@
-from django.test import TestCase
-
-# Create your tests here.
-class SimpleTest(TestCase):
-	def test_index(self):
-		resp = self.client.get('/AdminUnit/')
-		self.assertEqual(resp.status_code,200)
--- AdminUnit/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from AdminUnit.models import UserProfile, Event, Job, Organization, Shift
-from django.contrib.auth.models import User
-
-admin.site.register(UserProfile)
-admin.site.register(Event)
-admin.site.register(Job)
-admin.site.register(Organization)
-admin.site.register(Shift)
