055a8db7089711f30781a9c1073b44af437fb27c
jayesh92 <jlahori92@gmail.com>
2015-06-02 15:09:10 +0530

    Skeleton selenium code for functional testing in home app

--- vms/administrator/admin.py
+++ vms/administrator/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from models import Administrator
+
+admin.site.register(Administrator)
--- vms/home/tests.py
+++ vms/home/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from django.contrib.staticfiles.testing import LiveServerTestCase
 
-# Create your tests here.
+from django.contrib.auth.models import User
+
+from selenium import webdriver
+
+class AdminTestCase(LiveServerTestCase):
+    def setUp(self):
+        User.objects.create_superuser(
+                username = 'admin',
+                password = 'admin',
+                email = 'admin@admin.com'
+                )
+
+        self.selenium = webdriver.Firefox()
+        self.selenium.maximize_window()
+        super(AdminTestCase, self).setUp()
+
+    def tearDown(self):
+        self.selenium.quit()
+        super(AdminTestCase, self).tearDown()
+
+    def test_admin_site(self):
+        pass
